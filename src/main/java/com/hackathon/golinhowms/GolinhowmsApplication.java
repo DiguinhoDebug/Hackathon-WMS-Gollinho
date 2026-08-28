8af9bafc4803bb566333fcfcb7c7bb1ce653a5dd
SAMUEL EDUARDO DA LUZ KOHLER MESQUITA <samuel_mesquita@estudante.sesisenai.org.br>
2026-08-28 20:35:59 -0300

    first commit

---
+++ src/main/java/com/hackathon/golinhowms/GolinhowmsApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.golinhowms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GolinhowmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GolinhowmsApplication.class, args);
+	}
+
+}

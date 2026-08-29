52396fa2a058a2532d5ebfe629c94710d8d3df7c
SAMUEL EDUARDO DA LUZ KOHLER MESQUITA <samuel_mesquita@estudante.sesisenai.org.br>
2026-08-28 23:20:11 -0300

    feat: consulta simples no produto repository

--- src/main/java/com/hackathon/golinhowms/repository/ProdutoRepository.java
+++ src/main/java/com/hackathon/golinhowms/repository/ProdutoRepository.java
@@ -3,6 +3,9 @@ package com.hackathon.golinhowms.repository;
 import com.hackathon.golinhowms.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+import java.util.List;
 
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+    List<Produto> findByNome(String nome);
+    Produto findByIdProduto(Long idProduto);
 }

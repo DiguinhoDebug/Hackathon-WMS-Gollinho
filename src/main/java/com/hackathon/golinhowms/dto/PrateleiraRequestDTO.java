9e6d0327ee3cfdfca82eff5df8d565aa23ea6dff
KAIO GALVIN <kaio_galvin@estudante.sesisenai.org.br>
2026-08-29 01:50:57 -0300

    to fazendo o DTO

--- src/main/java/com/hackathon/golinhowms/dto/PrateleiraRequestDTO.java
+++ src/main/java/com/hackathon/golinhowms/dto/PrateleiraRequestDTO.java
@@ -6,10 +6,12 @@ public record PrateleiraRequestDTO(
 
         @NotNull(message = "Informe o andar da prateleira")
         Integer andar,
-        @NotNull(message = "Informe o andar da prateleira")
+        @NotNull(message = "Informe o coluna da prateleira")
         Integer coluna,
-        @NotNull(message = "Informe o andar da prateleira")
+        @NotNull(message = "Informe o corredor do produto")
         Integer corredor,
-        String nomeProduto,
+        @NotNull (message = "informe o id")
+        String idProduto,
+        @NotNull(message = "Informe a quantidade")
         Integer quantidade
 ) {}

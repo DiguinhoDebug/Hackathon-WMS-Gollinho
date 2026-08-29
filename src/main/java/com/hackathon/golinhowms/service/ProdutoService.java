3531f49f60768ef7073a1b6682aa068a79dceda1
SAMUEL EDUARDO DA LUZ KOHLER MESQUITA <samuel_mesquita@estudante.sesisenai.org.br>
2026-08-29 00:03:42 -0300

    feat: Adicionei repository do fornecedor

--- src/main/java/com/hackathon/golinhowms/service/ProdutoService.java
+++ src/main/java/com/hackathon/golinhowms/service/ProdutoService.java
@@ -2,18 +2,21 @@ package com.hackathon.golinhowms.service;
 
 import com.hackathon.golinhowms.dto.ProdutoRequestDTO;
 import com.hackathon.golinhowms.dto.ProdutoResponseDTO;
+import com.hackathon.golinhowms.exception.RecursoNaoEncontrado;
 import com.hackathon.golinhowms.model.Fornecedor;
 import com.hackathon.golinhowms.model.Produto;
+import com.hackathon.golinhowms.repository.FornecedorRepository;
 import com.hackathon.golinhowms.repository.ProdutoRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProdutoService {
     private final ProdutoRepository produtoRepository;
+    private final FornecedorRepository fornecedorRepository;
 
-
-    public ProdutoService(ProdutoRepository produtoRepository){
+    public ProdutoService(ProdutoRepository produtoRepository, FornecedorRepository fornecedorRepository){
         this.produtoRepository = produtoRepository;
+        this.fornecedorRepository = fornecedorRepository;
     }
 
     private ProdutoResponseDTO toResponse(Produto produto){
@@ -29,9 +32,9 @@ public class ProdutoService {
     private Produto toEntity(ProdutoRequestDTO dto){
         Produto produto = new Produto();
 
-        Fornecedor fornecedor = fornecedorRepository
+        Fornecedor fornecedor = fornecedorRepository.findById(dto.idFornecedor()).orElseThrow(() -> new RecursoNaoEncontrado(""));
 
         produto.setNome(dto.nomeProduto());
-        produto.setFornecedor(dto.idFornecedor());
+        produto.setFornecedor(fornecedor);
     }
 }
